d30e86c959ea28c39d80c9b0b6479f54fec02888
shudh25 <pandey252001@gmail.com>
2023-08-12 17:24:33 +0530

    Interacting with forms

---
+++ reactTut04/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Meme from "./components/Memes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
